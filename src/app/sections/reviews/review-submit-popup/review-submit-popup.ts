5f4620e7e7e4f62e8982549b20416f7df4276e02
hirudilsh-dev <hirudilsh.all@gmail.com>
2026-01-03 01:03:25 +0530

    Review submit popup

--- src/app/sections/reviews/review-submit-popup/review-submit-popup.ts
+++ src/app/sections/reviews/review-submit-popup/review-submit-popup.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-review-submit-popup',
-  imports: [],
+  imports: [
+    CommonModule,
+    DialogModule,
+    InputTextModule,
+    InputTextModule
+  ],
   templateUrl: './review-submit-popup.html',
   styleUrl: './review-submit-popup.css',
 })
 export class ReviewSubmitPopup {
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
 
 }
