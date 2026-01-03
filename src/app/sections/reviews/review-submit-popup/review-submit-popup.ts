95ef87141aaa0435f5b6a8f2823d6fc893dbd74d
hirudilsh-dev <hirudilsh.all@gmail.com>
2026-01-03 12:22:37 +0530

    Review submit via google sheet

--- src/app/sections/reviews/review-submit-popup/review-submit-popup.ts
+++ src/app/sections/reviews/review-submit-popup/review-submit-popup.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 
@@ -9,17 +11,61 @@ import { InputTextModule } from 'primeng/inputtext';
     CommonModule,
     DialogModule,
     InputTextModule,
-    InputTextModule
+    InputTextModule,
+    FormsModule
   ],
   templateUrl: './review-submit-popup.html',
   styleUrl: './review-submit-popup.css',
 })
 export class ReviewSubmitPopup {
 
+  constructor(
+    private http: HttpClient,
+    private cd: ChangeDetectorRef
+  ) {}
+
   visible: boolean = false;
+
+  reviewData = {
+    name: '',
+    location: '',
+    rating: '5',
+    review: ''
+  };
 
   showDialog() {
     this.visible = true;
+  }
+
+  onSubmit() {
+    this.submitToSheet();
+  }
+
+  submitToSheet() {
+    //Deployment ID - AKfycbz-wYc-p8aYChY7EvC39ORGGTKhdYM4RctBKqdassZ16kHOAbeKmK9fGrP_rlYCA_Muew
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz-wYc-p8aYChY7EvC39ORGGTKhdYM4RctBKqdassZ16kHOAbeKmK9fGrP_rlYCA_Muew/exec';
+    
+    // Header එකක් අවශ්‍ය නැත, content-type එක plain text ලෙස යැවීමෙන් 
+    // බොහෝ විට CORS ගැටළු මගහැරේ
+    this.http.post(scriptURL, JSON.stringify(this.reviewData), {
+      responseType: 'text' 
+    }).subscribe({
+      next: (res) => {
+        console.log('Success!', res);
+        this.visible = false;
+        this.cd.detectChanges();
+        alert('Review added successfully!');
+      },
+      error: (err) => {
+        // මෙහිදී status 0 ආවත් සමහරවිට sheet එක update වී තිබිය හැක
+        console.error('Submission Error:', err);
+        // Status 0 යනු response එක block වීමක් මිස data නොයෑමම නොවේ
+        if (err.status === 0) {
+          alert('Review submitted! (Note: Redirect handled by browser)');
+          this.visible = false;
+        }
+      }
+    });
   }
 
 }
